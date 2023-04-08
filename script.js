0034378dd92d986b50232a4f8f2c93e1f5084049
ElWaje <105492177+ElWaje@users.noreply.github.com>
2023-04-08 12:26:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,106 @@
-console.log('Hello!');
+const audio = document.getElementById('audio');
+const playButton = document.getElementById('play-button');
+const pauseButton = document.getElementById('pause-button');
+const volumeControl = document.getElementById('volume-control');
+
+playButton.addEventListener('click', () => {
+  audio.play();
+});
+
+pauseButton.addEventListener('click', () => {
+  audio.pause();
+});
+
+volumeControl.addEventListener('input', () => {
+  audio.volume = volumeControl.value;
+});
+
+function toggleSectionVisibility(sectionId) {
+  const section = document.getElementById(sectionId);
+  const separador = document.querySelector('.separador');
+
+  section.classList.toggle('oculto');
+
+  if (
+    !document.getElementById('seccion1').classList.contains('oculto') &&
+    !document.getElementById('seccion2').classList.contains('oculto')
+  ) {
+    separador.classList.remove('oculto');
+  } else {
+    separador.classList.add('oculto');
+  }
+}
+
+const seccion1 = document.getElementById('seccion1');
+seccion1.classList.add('oculto');
+
+const seccion2 = document.getElementById('seccion2');
+seccion2.classList.add('oculto');
+
+const separador = document.querySelector('.separador');
+separador.classList.add('oculto');
+
+const navSeccion1 = document.getElementById('nav-seccion1');
+navSeccion1.addEventListener('click', (event) => {
+  event.preventDefault();
+  toggleSectionVisibility('seccion1');
+  if (!seccion1.classList.contains('oculto') || !seccion2.classList.contains('oculto')) {
+    separador.classList.remove('oculto');
+  } else {
+    separador.classList.add('oculto');
+  }
+});
+
+const navSeccion2 = document.getElementById('nav-seccion2');
+navSeccion2.addEventListener('click', (event) => {
+  event.preventDefault();
+  toggleSectionVisibility('seccion2');
+  if (!seccion1.classList.contains('oculto') || !seccion2.classList.contains('oculto')) {
+    separador.classList.remove('oculto');
+  } else {
+    separador.classList.add('oculto');
+  }
+});
+
+function loadVideo(videoSrc, imageSrc) {
+  const video = document.querySelector(`img[src="${imageSrc}"] + video`);
+  if (video) {
+    video.src = videoSrc;
+    video.style.display = "block";
+  }
+}
+
+const seccionesContainer = document.querySelector('.secciones-container');
+seccionesContainer.style.display = "flex";
+
+const seccion1Img = seccion1.querySelector('img');
+const seccion1Video = seccion1.querySelector('video');
+
+const seccion2Img = seccion2.querySelector('img');
+const seccion2Video = seccion2.querySelector('video');
+
+seccion1Img.style.width = "200px";
+seccion1Img.style.height = "200px";
+seccion1Img.style.margin = "20px";
+seccion1Video.style.width = "200px";
+seccion1Video.style.height = "200px";
+seccion1Video.style.margin = "20px";
+seccion1.style.flex = "1";
+seccion1.style.display = "flex";
+seccion1.style.justifyContent = "center";
+seccion1.style.alignItems = "center";
+seccion1.style.flexDirection = "column";
+seccion1.style.margin = "0 10%";
+
+seccion2Img.style.width = "200px";
+seccion2Img.style.height = "200px";
+seccion2Img.style.margin = "20px";
+seccion2Video.style.width = "200px";
+seccion2Video.style.height = "200px";
+seccion2Video.style.margin = "20px";
+seccion2.style.flex = "1";
+seccion2.style.display = "flex";
+seccion2.style.justifyContent = "center";
+seccion2.style.alignItems = "center";
+seccion2.style.flexDirection = "column";
+seccion2.style.margin = "0 10%"; 
