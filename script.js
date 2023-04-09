08bbe048aa1d8cae84c8c20bfc6eacc8c0e5c5a3
ElWaje <105492177+ElWaje@users.noreply.github.com>
2023-04-09 23:54:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,84 +1,73 @@
-const audio = document.getElementById('audio');
-const playButton = document.getElementById('play-button');
-const pauseButton = document.getElementById('pause-button');
-const volumeControl = document.getElementById('volume-control');
-
-playButton.addEventListener('click', () => {
-  audio.play();
-});
-
-pauseButton.addEventListener('click', () => {
-  audio.pause();
-});
-
-volumeControl.addEventListener('input', () => {
-  audio.volume = volumeControl.value;
-});
-
-function toggleSectionVisibility(sectionId) {
-  const section = document.getElementById(sectionId);
-  const separador = document.querySelector('.separador');
+document.addEventListener('DOMContentLoaded', () => {
+  const audio = document.getElementById('audio');
+  const playButton = document.getElementById('play-button');
+  const pauseButton = document.getElementById('pause-button');
+  const volumeControl = document.getElementById('volume-control');
   const seccion1 = document.getElementById('seccion1');
   const seccion2 = document.getElementById('seccion2');
+  const navSeccion1 = document.getElementById('nav-seccion1');
+  const navSeccion2 = document.getElementById('nav-seccion2');
+  const separador = document.querySelector('.separador');
+  const seccionesContainer = document.querySelector('.secciones-container');
+    
+  playButton.addEventListener('click', () => {
+    audio.play();
+  });
 
-  section.classList.toggle('oculto');
+  pauseButton.addEventListener('click', () => {
+    audio.pause();
+  });
 
-  if (!seccion1.classList.contains('oculto') && !seccion2.classList.contains('oculto')) {
-    separador.classList.remove('oculto');
-  } else {
-    separador.classList.add('oculto');
+  volumeControl.addEventListener('input', () => {
+    audio.volume = volumeControl.value;
+  });
+
+  function toggleSectionVisibility(section) {
+    section.classList.toggle('seccion-oculta');
+    const seccion1Visible = !seccion1.classList.contains('seccion-oculta');
+    const seccion2Visible = !seccion2.classList.contains('seccion-oculta');
+    if (seccion1Visible && seccion2Visible) {
+      separador.classList.remove('separador-oculto');
+    } else {
+      separador.classList.add('separador-oculto');
+    }
+    // Agregar/quitar clase "active" según corresponda
+    if (section.id === "seccion1") {
+      if (seccion1Visible) {
+        navSeccion1.classList.add("active");
+      } else {
+        navSeccion1.classList.remove("active");
+      }
+    } else if (section.id === "seccion2") {
+      if (seccion2Visible) {
+        navSeccion2.classList.add("active");
+      } else {
+        navSeccion2.classList.remove("active");
+      }
+    }
   }
-}
 
-const seccion1 = document.getElementById('seccion1');
-seccion1.classList.add('oculto');
+  seccion1.style.width = '45%';
+  seccion2.style.width = '45%';
+  separador.style.width = '10px';
+  separador.style.height = seccionesContainer.clientHeight + 'px';
+  separador.style.margin = '0 20px';
+  separador.style.backgroundColor = 'pink';
 
-const seccion2 = document.getElementById('seccion2');
-seccion2.classList.add('oculto');
+  // Ocultar seccion1 y seccion2 al cargar la página
+  seccion1.classList.add('seccion-oculta');
+  seccion2.classList.add('seccion-oculta');
 
-const separador = document.querySelector('.separador');
-separador.classList.add('oculto');
+  // Añadir evento de click al botón de la seccion1
+  navSeccion1.addEventListener('click', (event) => {
+    event.preventDefault();
+    toggleSectionVisibility(seccion1);
+  });
 
-const navSeccion1 = document.getElementById('nav-seccion1');
-navSeccion1.addEventListener('click', (event) => {
-  event.preventDefault();
-  toggleSectionVisibility('seccion1');
+  // Añadir evento de click al botón de la seccion2   
+  navSeccion2.addEventListener('click', (event) => {
+    event.preventDefault();
+    toggleSectionVisibility(seccion2);
+  }); 
+   
 });
-
-const navSeccion2 = document.getElementById('nav-seccion2');
-navSeccion2.addEventListener('click', (event) => {
-  event.preventDefault();
-  toggleSectionVisibility('seccion2');
-});
-
-const seccion1Img = seccion1.querySelector('img');
-const seccion1Video = seccion1.querySelector('video');
-
-const seccion2Img = seccion2.querySelector('img');
-const seccion2Video = seccion2.querySelector('video');
-
-seccion1Img.style.width = "200px";
-seccion1Img.style.height = "200px";
-seccion1Img.style.margin = "20px";
-seccion1Video.style.width = "200px";
-seccion1Video.style.height = "200px";
-seccion1Video.style.margin = "20px";
-seccion1.style.flex = "1";
-seccion1.style.display = "flex";
-seccion1.style.justifyContent = "center";
-seccion1.style.alignItems = "center";
-seccion1.style.flexDirection = "column";
-seccion1.style.margin = "0 10%";
-
-seccion2Img.style.width = "200px";
-seccion2Img.style.height = "200px";
-seccion2Img.style.margin = "20px";
-seccion2Video.style.width = "200px";
-seccion2Video.style.height = "200px";
-seccion2Video.style.margin = "20px";
-seccion2.style.flex = "1";
-seccion2.style.display = "flex";
-seccion2.style.justifyContent = "center";
-seccion2.style.alignItems = "center";
-seccion2.style.flexDirection = "column";
-seccion2.style.margin = "0 10%";
